2b935258a583116d6c6666aa17e798ddb18a33f7
Justin Joyce <justin.joyce@joocer.com>
2020-03-08 12:04:18 +0000

    some methods use comprehension instead of loops

--- timeseries/general.py
+++ timeseries/general.py
@@ -24,20 +24,14 @@ def variance(series):
 
 # executes a rule against each item in a series and returns matches, rule should be a lambe
 # matches(data, lambda x: x > 2)
+# list comprehension is slower compared to loops for shorter lists but faster for longer lists
 def matches(series, rule):
-    indices = []
-    for i in range(len(series)):
-        if rule(series[i]):
-            indices.append(i)
-    return indices
+    return [i for i, item in enumerate(series) if rule(item)]
 
 # applied a function to a series of values, formula should be a lambda
 # f_x(data, lambda x: 3x + 2)
 def f_x(series, formula):
-    r = []
-    for i in range(len(series)):
-        r.append(formula(series[i]))
-    return r
+    return [formula(item) for item in series]
 
 def series_diff(series_a, series_b, adjustment=None):
     if len(series_a) != len (series_b):
